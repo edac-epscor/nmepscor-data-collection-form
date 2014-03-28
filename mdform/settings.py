cc69c36adb41f861cbc165a239670b4dd0d22a34
Jason Brown <jbrown@edac.unm.edu>
2014-03-28 16:28:07 -0600

    Remove purged libs

--- mdform/settings.py
+++ mdform/settings.py
@@ -144,13 +144,10 @@ BOWER_INSTALLED_APPS = (
     'modernizr#2.6.2',
     'respond#1.1.0',
 
-    #'angular#1.0.8',
-    #'angular-cookies#1.0.8',
-    #'angular-sanitize#1.0.8',  # upgraded
-    'angular#1.2.1',
-    'angular-cookies#1.2.1',
-    'angular-sanitize#1.2.1',  # upgraded
-    'angular-route#1.2.1',  # upgraded
+    'angular#1.2.15',
+    'angular-cookies#1.2.15',
+    'angular-sanitize#1.2.15',  # upgraded
+    'angular-route#1.2.15',  # upgraded
 
     'bootstrap#3.0.0',
     'angular-ui-bootstrap-bower#0.1.0',  # 2xcheck
@@ -162,8 +159,6 @@ BOWER_INSTALLED_APPS = (
     'angular-xeditable#0.1.7',  # choose 3.0.0 in bower
 
     'underscore#1.4.4',  # upgraded
-    #'ace-builds#1.1.1',  # we were cdnjs 1.1.01
-    #'angular-ui-ace#0.1.0',  # upgraded
     #'google-code-prettify',  # the bower finds are...weird...sticking with cdnjs right now
     'angular-strap#0.7.5',  # choose 3.0.0 in bower
 
